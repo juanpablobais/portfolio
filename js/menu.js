2b167e3fd25e48f4b7d4e35ae2fc327564148bbb
Juan Pablo Bais <juanpablobais@gmail.com>
2021-11-24 23:20:50 -0300

    Portfolio commit

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamburger')
+const menu = document.querySelector('.menuNavegation')
+
+console.log(menu)
+console.log(hamburger)
+
+hamburger.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread') && e.target != menu && e.target != hamburger  ){
+
+
+            menu.classList.toggle("spread")
+    }
+})
